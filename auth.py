44cc5bc788b6e1bf42f8b996c8440c18ca113a23
Kai Devrim <kkgithub@devrim.tech>
2020-08-08 21:21:22 -0700

    initial commit, setup auth

---
+++ auth.py
@@ -0,0 +1,9 @@
+import schoolopy
+import yaml
+from dotenv import load_dotenv
+import os
+load_dotenv()
+
+sc = schoolopy.Schoology(schoolopy.Auth(
+    os.getenv("schoolKey"), os.getenv("schoolSecret")))
+sc.limit = 10  # Only retrieve 10 objects max
